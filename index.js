f8e037b56614f4d801cd1503f70a43bd8c426a43
christian landsberger Cglik001@gmail.com <cglik001@gmail.com>
2022-07-20 20:03:07 -0300

    lab intro-node done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a,b) {
+      return a - b
+    })
 
-  get(pos) {}
+    this.length = this.items.length
 
-  max() {}
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutofBounds')
+    }
+    else {
+       return this.items[pos]
+    }
+  }
 
-  avg() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error ("EmptySortedList")
+    }
+    else {
+      return this.items[this.items.length - 1]
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error ("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    else {
+        return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    }
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error ("Empty Sorted List")
+    } else {
+      return this.sum() / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
